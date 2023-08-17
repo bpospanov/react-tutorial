6ef990f6b9ac24728d5e80b62ad8734c280f22a8
Bakhtiyar Ospanov <b.p.ospanov@gmail.com>
2023-08-17 21:06:53 +0600

    multiple components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  const link = 'http://www.googl.com';
-
   return (
-    <div className="App">
-      <div className="content">
-        <h1>{ title }</h1>
-        <p> Liked { likes } times</p>
-        {/* <p>{ person }</p> */}
-        <p>{ [1, 2, 3, 4] }</p>
-        <p>{ Math.random() * 10 }</p>
-
-
-        <a href={link}>Google site</a>
+      <div className="App">
+        <Navbar/>
+        <Home/>
       </div>
-    </div>
   );
 }
 
