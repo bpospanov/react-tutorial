34c75c3c8b2e1575b502bfe102b85a6bd987ab7d
Bakhtiyar Ospanov <b.p.ospanov@gmail.com>
2023-08-18 09:11:05 +0600

    submit events

--- src/Create.js
+++ src/Create.js
@@ -5,10 +5,16 @@ const Create = () => {
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = {title, body, author};
+    console.log(blog);
+  };
+
   return (
       <div className="create">
         <h1>Add a new blog</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>Blog title:</label>
           <input
               type="text"
