612dd6e116a8532e77be30057137f4d690414a59
Bakhtiyar Ospanov <b.p.ospanov@gmail.com>
2023-08-18 09:17:51 +0600

    making a post request

--- src/Create.js
+++ src/Create.js
@@ -4,11 +4,20 @@ const Create = () => {
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
+  const [isPending, setIsPending] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const blog = {title, body, author};
-    console.log(blog);
+    setIsPending(true);
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(blog),
+    }).then(r => {
+      console.log('Blog created!');
+      setIsPending(false);
+    });
   };
 
   return (
@@ -36,7 +45,8 @@ const Create = () => {
             <option value="mario">mario</option>
             <option value="yoshi">yoshi</option>
           </select>
-          <button>Add Blog</button>
+          {!isPending && <button>Add Blog</button>}
+          {isPending && <button disabled>Add blog...</button>}
         </form>
       </div>
   );
